5da518a580e7d8ae3c014c83f6f72c2846fd4a82
Comedyman937 <comedyman937@yahoo.com>
2014-08-25 13:08:41 -0400

    Better Password Security

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 session_start();
 ob_start();
 
+require_once 'config/conf.php';
+
 $isLoggedIn = false;
 
 require_once 'assets/includes/header.php';
@@ -22,8 +24,14 @@ require_once 'assets/includes/nav.php';
 				<hr>
 				<h2>File Management</h2>
 				<p>
-					<a type="button" class="btn btn-success" href="#files">Files </a>
+					<a type="button" class="btn btn-success" data-toggle="modal" data-target="#files">Files </a>
 				</p>
+				<hr>
+				<h2>BootPanel Scripts</h2>
+				<p>
+					<a type="button" class="btn btn-info" data-toggle="modal" data-target="#update">Update</a>
+				</p>
+				<hr>
 			</center>
 		</div>
 	</div>
@@ -38,19 +46,74 @@ require_once 'assets/includes/nav.php';
 				</div>
 				<div class="modal-body">
 					<center>
-						<p>
-							<?php
-								if($isLoggedIn) {
-									?>
-										<!-- TODO: Show Files -->
-									<?php
-								}else{
-									?>
-										<!-- TODO: Force login -->
-									<?php
+						<?php
+							if($isLoggedIn) {
+								?>
+									<!-- TODO: Show Files -->
+								<?php
+							}else{
+								?>
+									<p class="alert alert-danger">You must be signed in to do that</p>
+									<p><a type="button" class="btn btn-default" data-toggle="modal" data-target="#login">Sign In</a></p>
+								<?php
+							}
+						?>
+					</center>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<!-- Show Login -->
+	<div class="modal fade" id="login" tabindex="-1" role="dialog" aria-labelledby="loginLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+					<center><h4 class="modal-title" id="loginLabel">Sign In</h4></center>
+				</div>
+				<div class="modal-body">
+					<center>
+						<?php
+							if($isLoggedIn) {
+						?>
+									<p class="alert alert-success">You have been logged in!</p>
+						<?php
+							}else{
+								if(isset($_POST['username']) && isset($_POST['password'])) {
+									$username = strtolower($_POST['username']);
+									$password = $_POST['password'];
+									if($username == $ROOT_USER) {
+										if($password == $ROOT_PASS) {
+											$isLoggedIn = true;
+						?>
+											<p class="alert alert-success">You have been logged in!</p>
+						<?php
+										}else{
+						?>
+											<p class="alert alert-danger">Invalid Username/Password!</p>
+						<?php
+										}
+									}else{
+						?>
+										<p class="alert alert-danger">Invalid Username/Password!</p>
+						<?php
+									}
 								}
-							?>
-						</p>
+						?>
+								<div class="container">
+     								<form class="form-signin" role="form">
+        								<input type="username" name="username" class="form-control" placeholder="Username" required="" autofocus="">
+        								<input type="password" name="password" class="form-control" placeholder="Password" required="">
+        								<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      								</form>
+    							</div>
+						<?php
+							}
+						?>
 					</center>
 				</div>
 				<div class="modal-footer">
