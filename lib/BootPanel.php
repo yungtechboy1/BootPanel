a788b6297672e3486b1df0bb317befdd545b8b30
root <root@f22711318734>
2015-02-17 21:51:21 +0000

    modified:   lib/BootPanel.php 	modified:   panel.php

--- lib/BootPanel.php
+++ lib/BootPanel.php
@@ -13,19 +13,22 @@
 	
 	class BootPanel {
 		const VERSION = "1.1.0";
-		const API_VERSION = 0;
+		const API_VERSION = 0.1;
 		
 		public static function __init($bypass_login = false) {
 			static $init_called;
 			if(!$init_called) {
-				if(is_dir("./themes") && is_dir("./plugins")) {
-					$init_called = true;
-					if($bypass_login)
-						BootPanel::getAPI()->getAuth()->doLogin("BootPanel", BootPanel::getConfig("BootPanel")->get("Password", "Username='BootPanel'"));
-					ThemeLoader::loadTheme();
-					PluginLoader::loadPlugins();
+				if(BootPanel::extensionsLoaded()) {
+					if(is_dir("./themes") && is_dir("./plugins")) {
+						$init_called = true;
+						if($bypass_login)
+							BootPanel::getAPI()->getAuth()->doLogin("BootPanel", BootPanel::getConfig("BootPanel")->get("Password", "Username='BootPanel'"));
+						ThemeLoader::loadTheme();
+						PluginLoader::loadPlugins();
+					} else
+						BootPanel::__create();
 				} else
-					BootPanel::__create();
+					header('Location: http://error.BootPanel.net/missing-extensions');
 			}
 		}
 		
@@ -41,8 +44,15 @@
 			return bin2hex(hash("sha512", $string ."&<421#1$+09=L%acx*", true) ^ hash("whirlpool", "&<421#1$+09=L%acx*". $string, true));
 		}
 		
+		private static function extensionsLoaded() {
+			if(!extension_loaded("sqlite3"))
+				return false;
+			return true;
+		}
+		
 		private static function __create() {
 			@mkdir("./themes");
 			@mkdir("./plugins");
+			header('Location: ./');
 		}
 	}
--- panel.php
+++ panel.php
@@ -10,16 +10,4 @@
 		@unlink("./BootPanel.phar");
 		header("Location: ./");
 	} else
-		die('<html>
-				<head>
-					<title>BootPanel | ERROR</title>
-				</head>
-				<body>
-					<center>
-						<h1>No BootPanel Installation Found!</h1><hr>
-						No \'BootPanel.phar\' file could be found in \''. __DIR__ . DIRECTORY_SEPARATOR .'\'!<br>
-						Please upload a \'BootPanel.phar\' file to your BootPanel Directory (Shown Above) and try again.<br><br>
-						Download the most recent \'BootPanel.phar\' file <a target="_blank" href="http://download.BootPanel.net/BootPanel.phar">HERE</a>
-					</center>
-				</body>
-			 </html>');
+		header('Location: http://error.BootPanel.net/no-installation');
